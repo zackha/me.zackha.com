3136d5d13f9df7cd876d5fe4aac7bac482f6b1f0
Zack Hatlen <zckhtln@icloud.com>
2023-01-12 16:13:15 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <div class="main">
       <div class="time">{{ currentTime }}</div>
       <div class="about">
-        <div class="face"><img src="face.png"></div>
+        <div class="face"><img src="/face.png"></div>
         <div class="name">Zack Hatlen</div>
         <div class="info"> I am a frontend engineer focused on modern web technologies such as <a target="_blank" href="https://vuejs.org/guide/introduction.html">Vue</a>, <a target="_blank" href="https://reactjs.org/docs/getting-started.html">React</a>, <a target="_blank" href="https://nuxt.com/docs/getting-started/introduction">Nuxt</a>, <a target="_blank" href="https://graphql.org/learn/">GraphQL</a>, and serverless computing. Usually, I work with eCommerce technologies in <a target="_blank" href="https://woocommerce.github.io/woocommerce-rest-api-docs/">Woocommerce</a>. </div>
       </div>
