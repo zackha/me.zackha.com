a3854fcda0a37b6c3775ef2ba402f2810d68263c
Zack Hatlen <zckhtln@icloud.com>
2023-01-14 10:46:44 +0300

    master

--- app.vue
+++ app.vue
@@ -5,11 +5,11 @@
       <div class="about">
         <div class="face"><img src="/face.png"></div>
         <div class="name">Zack Hatlen</div>
-        <div class="info"> I am a frontend engineer focused on modern web technologies such as <a target="_blank" href="https://vuejs.org/guide/introduction.html">Vue</a>, <a target="_blank" href="https://reactjs.org/docs/getting-started.html">React</a>, <a target="_blank" href="https://nuxt.com/docs/getting-started/introduction">Nuxt</a>, <a target="_blank" href="https://graphql.org/learn/">GraphQL</a>, and serverless computing. Usually, I work with eCommerce technologies in <a target="_blank" href="https://woocommerce.github.io/woocommerce-rest-api-docs/">Woocommerce</a>. </div>
+        <div class="info"> I am a frontend engineer and designer focused on modern web technologies such as <a target="_blank" href="https://vuejs.org/guide/introduction.html">Vue</a>, <a target="_blank" href="https://reactjs.org/docs/getting-started.html">React</a>, <a target="_blank" href="https://nuxt.com/docs/getting-started/introduction">Nuxt</a>, <a target="_blank" href="https://graphql.org/learn/">GraphQL</a> and serverless computing. Usually, I work with eCommerce technologies in <a target="_blank" href="https://woocommerce.github.io/woocommerce-rest-api-docs/">Woocommerce</a>.</div>
       </div>
       <div class="contact">
         <a target="_blank" href="https://www.github.com/zackha/">github</a>
-        <a target="_blank" href="mailto:zckhtln@icloud.com">email</a>
+        <a target="_blank" href="mailto:zckhtln@icloud.com?subject=I want you">email</a>
         <a target="_blank" href="">cv</a>
       </div>
     </div>
@@ -40,6 +40,7 @@ html, body {
   margin: 0;
   font-family: Courier New,Courier,Monaco;
   font-size: 11px;
+  background-color: #f2f2f2;
 }
 .content {
   display: flex;
@@ -56,30 +57,29 @@ html, body {
 .time {
   font-size: 12px;
   text-align: center;
-  margin-bottom: 1.5rem;
+  margin: auto auto 3.5rem auto;
 }
 .about {
-  border: 1px dotted #666;
-  border-radius: 12px;
+  border: 1px dashed #666;
+  border-radius: 22px;
+  background-color: #fff;
+  box-shadow: 0px 0px 0px 12px #fff;
+  padding: 12px;
 }
 .face {
-  height: 206px;
-  width: 206px;
-  margin: 3px 0px 15px 3px;
+  text-align: center;
+  height: 188px;
 }
 .face img {
-  width: 206px;
-  height: 206px;
-  border-radius: 8px 8px 0 0;
+  width: 188px;
+  height: 188px;
+  border-radius: 12px;
+  box-shadow: 0px 0px 20px -5px rgb(0 0 0 / 40%);
 }
 .name {
   font-weight: 600;
   font-size: 15px;
-  margin-bottom: 3px;
-  padding: 0 15px;
-}
-.info {
-  padding: 0 15px 15px 15px;
+  margin: 12px 0px 3px;
 }
 .info a {
   color: #000;
@@ -88,7 +88,7 @@ html, body {
 }
 .contact {
   text-align: center;
-  margin-top: 1.5rem;
+  margin: 3.5rem auto auto auto;
 }
 .contact a {
   padding: 2px 4px;
@@ -96,7 +96,11 @@ html, body {
   text-decoration: none;
   color: #000;
 }
-.contact a:hover,
+.contact a:hover {
+  background-color: red;
+  color: #fff;
+  border-radius: 5px;
+}
 .info a:hover {
   background-color: red;
   color: #fff;
