876d5b9ee816a4bd908084e9045823200f17cfa7
Zack Hatlen <zckhtln@icloud.com>
2023-01-16 11:08:15 +0300

    master
    Co-Authored-By: Elizabeth Source <122454875+elizabethsource@users.noreply.github.com>

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+export default {
+  target: 'static',
+  head: {
+    title: 'Zack Hatlen',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  components: true
+}
