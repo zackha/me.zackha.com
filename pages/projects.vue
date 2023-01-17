46dfd1d85e792cedd97a970f5a2209554f21568e
Zack Hatlen <zckhtln@icloud.com>
2023-01-17 19:01:59 +0300

    for-master
    Co-Authored-By: Elizabeth Source <122454875+elizabethsource@users.noreply.github.com>

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,15 +1,88 @@
 <template>
   <div class="projects">
-    <div class="project mb">— <a target="_blank" href="https://demo2-woonuxt.netlify.app/product/patterned-wrap-skirt" class="link">woonuxt.com</a></div>
-    <div class="project mb">— <a target="_blank" href="https://rays.com.tr/" class="link">rays.com.tr</a></div>
-    <div class="project mb">— <a target="_blank" href="https://terodon.com/" class="link">terodon.com</a></div>
-    <div class="project mb">— <a target="_blank" href="https://reybul.com/" class="link">reybul.com</a></div>
-    <div class="project mb">— <a target="_blank" href="https://github.com/zackha/woocommerce-admin" class="link">woocommerce-admin</a></div>
-    <div>— <a target="_blank" href="https://github.com/zackha/ra-data-woocommerce" class="link">ra-data-woocommerce</a></div>
+    <div class="project mb">
+      <a href="/images/woonuxt-bg.png" target="_blank"><img src="/images/woonuxt.png"></a>
+      <div class="description">
+        <div class="detail">Water resistant nylon with glitter vinyl yoke, down insulated quilted baffles and taffeta lining. Snap front closure with patch pockets at lower front and interior zip pocket.</div>
+        <a class="visit link"  target="_blank" href="https://demo2-woonuxt.netlify.app/product/patterned-wrap-skirt/">examine the project ></a>
+      </div>
+    </div>
+    <div class="project mb">
+      <a href="/images/rays-bg.png" target="_blank"><img src="/images/rays.png"></a>
+      <div class="description">
+        <div class="detail">Water resistant nylon with glitter vinyl yoke, down insulated quilted baffles and taffeta lining. Snap front closure with patch pockets at lower front and interior zip pocket.</div>
+        <a class="visit link"  target="_blank" href="https://rays.com.tr/">examine the project ></a>
+      </div>
+    </div>
+    <div class="project mb">
+      <a href="/images/terodon.png" target="_blank"><img src="/images/terodon.png"></a>
+      <div class="description">
+        <div class="detail">Water resistant nylon with glitter vinyl yoke, down insulated quilted baffles and taffeta lining. Snap front closure with patch pockets at lower front and interior zip pocket.</div>
+        <a class="visit link"  target="_blank" href="https://terodon.com/">examine the project ></a>
+      </div>
+    </div>
+    <div class="project mb">
+      <a href="/images/reybul-bg.png" target="_blank"><img src="/images/reybul.png"></a>
+      <div class="description">
+        <div class="detail">Water resistant nylon with glitter vinyl yoke, down insulated quilted baffles and taffeta lining. Snap front closure with patch pockets at lower front and interior zip pocket.</div>
+        <a class="visit link"  target="_blank" href="https://reybul.com/">examine the project ></a>
+      </div>
+    </div>
+    <div class="project mb">
+      <a href="https://github.com/zackha/woocommerce-admin/" target="_blank"><img src="/images/github.png"></a>
+      <div class="description">
+        <div class="detail">Water resistant nylon with glitter vinyl yoke, down insulated quilted baffles and taffeta lining. Snap front closure with patch pockets at lower front and interior zip pocket.</div>
+        <a class="visit link"  target="_blank" href="https://github.com/zackha/woocommerce-admin/">examine the project ></a>
+      </div>
+    </div>
+    <div class="project">
+      <a href="https://github.com/zackha/ra-data-woocommerce/" target="_blank"><img src="/images/github.png"></a>
+      <div class="description">
+        <div class="detail">Water resistant nylon with glitter vinyl yoke, down insulated quilted baffles and taffeta lining. Snap front closure with patch pockets at lower front and interior zip pocket.</div>
+        <a class="visit link"  target="_blank" href="https://github.com/zackha/ra-data-woocommerce/">examine the project ></a>
+      </div>
+    </div>
   </div>
 </template>
 <style>
-.project.mb {
-  margin-bottom: 11px
+.projects {
+  max-height: 348px;
+  overflow: auto;
+}
+.project {
+  display: flex;
+}
+.project img {
+  display: flex;
+  margin: 5px;
+  height: 120px;
+  border-radius: 6px;
+  box-shadow: 0px 0px 5px -2px rgb(0 0 0 / 0.19);
+}
+.mb {
+  margin-bottom: 11px;
+}
+.description {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 8px 0px 8px 8px;
+}
+.detail {
+  height: 95px;
+}
+.visit {
+  align-self: flex-end;
+}
+::-webkit-scrollbar {
+  width: 8px;
+}
+::-webkit-scrollbar-track {
+  background: #d6d6d6; 
+}
+::-webkit-scrollbar-thumb {
+  background: #b5b5b5; 
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #8f8f8f;
 }
 </style>
