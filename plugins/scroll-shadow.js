b3d82e59608d5b227e3fbce881af98acce6fba3d
Zack Hatlen <zckhtln@icloud.com>
2023-01-19 17:08:16 +0300

    Update scroll-shadow.js
    Co-Authored-By: Elizabeth Source <122454875+elizabethsource@users.noreply.github.com>

--- plugins/scroll-shadow.js
+++ plugins/scroll-shadow.js
@@ -5,11 +5,11 @@ var outer = document.querySelector(".scrollable-pane");
 if (pane && scroller && outer) {
   pane.addEventListener("scroll", function(){
     var scroll =  pane.scrollTop;
-    if (scroll < 10) {
+    if (scroll < 1) {
       outer.classList.add('top');
       outer.classList.remove('middle');
       outer.classList.remove('bottom');
-    } else if (scroll >= 10 && scroll < (scroller.scrollHeight - pane.offsetHeight) - 10) {
+    } else if (scroll >= 1 && scroll < (scroller.scrollHeight - pane.offsetHeight) - 1) {
       outer.classList.remove('top');
       outer.classList.add('middle');
       outer.classList.remove('bottom');
