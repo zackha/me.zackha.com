83dc90cad914b6c4fb29743dc4625ba20c4b1770
Zack Hatlen <zckhtln@icloud.com>
2023-07-03 18:38:36 +0300

    added new project

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,107 +1,212 @@
 <template>
   <div class="scrollable-pane top">
-  <div class="arrow-up"></div>
+    <div class="arrow-up"></div>
     <div class="pane">
       <div class="projects scrollable">
         <div class="project mb">
           <div class="image-wrapper">
+            <a href="https://nuxtcommerce.netlify.app" target="_blank">
+              <nuxt-img
+                format="webp"
+                width="300"
+                src="/images/nuxtcommerce.png"
+              />
+            </a>
+          </div>
+          <div class="description">
+            <div class="projectname">NuxtCommerce</div>
+            <div class="detail">
+              An open-source, dynamic e-commerce solution powered by Nuxt 3 and
+              GraphQL, headless storefront replacement for Woocommerce.
+            </div>
+            <a
+              class="visit link"
+              target="_blank"
+              href="https://nuxtcommerce.netlify.app"
+              >examine the project ></a
+            >
+          </div>
+        </div>
+        <div class="project mb">
+          <div class="image-wrapper">
             <a href="/images/woonuxt-bg.png" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/woonuxt.png"/>
+              <nuxt-img format="webp" width="300" src="/images/woonuxt.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">woonuxt.com</div>
-            <div class="detail">Developed by <a class="visit link"  target="_blank" href="https://github.com/scottyzen">@scottyzen</a> using Nuxt, GraphQL and WooCommerce, and I'm continuing to develop the second interface demo.</div>
-            <a class="visit link"  target="_blank" href="https://demo2-woonuxt.netlify.app/product/patterned-wrap-skirt/">examine the project ></a>
+            <div class="detail">
+              Developed by
+              <a
+                class="visit link"
+                target="_blank"
+                href="https://github.com/scottyzen"
+                >@scottyzen</a
+              >
+              using Nuxt, GraphQL and WooCommerce, and I'm continuing to develop
+              the second interface demo.
+            </div>
+            <a
+              class="visit link"
+              target="_blank"
+              href="https://demo2-woonuxt.netlify.app/product/patterned-wrap-skirt/"
+              >examine the project ></a
+            >
           </div>
         </div>
         <div class="project mb">
           <div class="image-wrapper">
             <a href="/images/rays-bg.png" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/rays.png"/>
+              <nuxt-img format="webp" width="300" src="/images/rays.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">rays.com.tr</div>
-            <div class="detail">ERP, POS integration and frontend/backend studies were done for a WooCommerce project using REST API, PHP and CSS.</div>
-            <a class="visit link"  target="_blank" href="https://rays.com.tr/">examine the project ></a>
+            <div class="detail">
+              ERP, POS integration and frontend/backend studies were done for a
+              WooCommerce project using REST API, PHP and CSS.
+            </div>
+            <a class="visit link" target="_blank" href="https://rays.com.tr/"
+              >examine the project ></a
+            >
           </div>
         </div>
         <div class="project mb">
           <div class="image-wrapper">
             <a href="/images/terodon.png" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/terodon.png"/>
+              <nuxt-img format="webp" width="300" src="/images/terodon.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">terodon.com</div>
-            <div class="detail">This is still in the process of being developed...</div>
-            <a class="visit link"  target="_blank" href="https://terodon.com/">examine the project ></a>
+            <div class="detail">
+              This is still in the process of being developed...
+            </div>
+            <a class="visit link" target="_blank" href="https://terodon.com/"
+              >examine the project ></a
+            >
           </div>
         </div>
         <div class="project mb">
           <div class="image-wrapper">
             <a href="/images/reybul-bg.png" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/reybul.png"/>
+              <nuxt-img format="webp" width="300" src="/images/reybul.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">reybul.com</div>
-            <div class="detail">Furniture Corporate Website developed using only HTML and CSS</div>
-            <a class="visit link"  target="_blank" href="https://reybul.com/">examine the project ></a>
+            <div class="detail">
+              Furniture Corporate Website developed using only HTML and CSS
+            </div>
+            <a class="visit link" target="_blank" href="https://reybul.com/"
+              >examine the project ></a
+            >
           </div>
         </div>
         <div class="project mb">
           <div class="image-wrapper">
             <a href="/images/zackha.png" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/zackha.png"/>
+              <nuxt-img format="webp" width="300" src="/images/zackha.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">zackha.com</div>
-            <div class="detail">This is still in the process of being developed...</div>
-            <a class="visit link"  target="_blank" href="https://zackha.com/">examine the project ></a>
+            <div class="detail">
+              This is still in the process of being developed...
+            </div>
+            <a class="visit link" target="_blank" href="https://zackha.com/"
+              >examine the project ></a
+            >
           </div>
         </div>
         <div class="project mb">
           <div class="image-wrapper">
-            <a href="https://github.com/zackha/woocommerce-admin/" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/github.png"/>
+            <a
+              href="https://github.com/zackha/woocommerce-admin/"
+              target="_blank"
+            >
+              <nuxt-img format="webp" width="300" src="/images/github.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">woocommerce-admin</div>
-            <div class="detail">An alternative open source woocommerce admin panel developed with <a class="visit link"  target="_blank" href="https://github.com/marmelab/react-admin">React Admin</a></div>
-            <a class="visit link"  target="_blank" href="https://github.com/zackha/woocommerce-admin/">examine the project ></a>
+            <div class="detail">
+              An alternative open source woocommerce admin panel developed with
+              <a
+                class="visit link"
+                target="_blank"
+                href="https://github.com/marmelab/react-admin"
+                >React Admin</a
+              >
+            </div>
+            <a
+              class="visit link"
+              target="_blank"
+              href="https://github.com/zackha/woocommerce-admin/"
+              >examine the project ></a
+            >
           </div>
         </div>
         <div class="project mb">
           <div class="image-wrapper">
             <a href="https://github.com/zackha/supaAuth/" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/supaAuth.png"/>
+              <nuxt-img format="webp" width="300" src="/images/supaAuth.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">supaAuth</div>
-            <div class="detail">A <a class="visit link" target="_blank" href="https://nuxt.com/">Nuxt3</a> + <a class="visit link" target="_blank" href="https://supabase.com/">Supabase</a> starter template. Add Login and Register page into your Nuxt 3 project using Supabase authentication.</div>
-            <a class="visit link"  target="_blank" href="https://github.com/zackha/supaAuth/">examine the project ></a>
+            <div class="detail">
+              A
+              <a class="visit link" target="_blank" href="https://nuxt.com/"
+                >Nuxt3</a
+              >
+              +
+              <a class="visit link" target="_blank" href="https://supabase.com/"
+                >Supabase</a
+              >
+              starter template. Add Login and Register page into your Nuxt 3
+              project using Supabase authentication.
+            </div>
+            <a
+              class="visit link"
+              target="_blank"
+              href="https://github.com/zackha/supaAuth/"
+              >examine the project ></a
+            >
           </div>
         </div>
         <div class="project">
           <div class="image-wrapper">
-            <a href="https://github.com/zackha/ra-data-woocommerce/" target="_blank">
-              <nuxt-img format="webp" width="300" src="/images/github.png"/>
+            <a
+              href="https://github.com/zackha/ra-data-woocommerce/"
+              target="_blank"
+            >
+              <nuxt-img format="webp" width="300" src="/images/github.png" />
             </a>
           </div>
           <div class="description">
             <div class="projectname">ra-data-woocommerce</div>
-            <div class="detail">Woocommerce REST API Data Provider for <a class="visit link"  target="_blank" href="https://github.com/marmelab/react-admin">react-admin</a>, the frontend framework for building admin applications on top of REST/GraphQL services.</div>
-            <a class="visit link"  target="_blank" href="https://github.com/zackha/ra-data-woocommerce/">examine the project ></a>
+            <div class="detail">
+              Woocommerce REST API Data Provider for
+              <a
+                class="visit link"
+                target="_blank"
+                href="https://github.com/marmelab/react-admin"
+                >react-admin</a
+              >, the frontend framework for building admin applications on top
+              of REST/GraphQL services.
+            </div>
+            <a
+              class="visit link"
+              target="_blank"
+              href="https://github.com/zackha/ra-data-woocommerce/"
+              >examine the project ></a
+            >
           </div>
         </div>
       </div>
     </div>
-  <div class="arrow-down"></div>
+    <div class="arrow-down"></div>
   </div>
 </template>
 
@@ -114,17 +219,19 @@ export default {
 
     if (pane && scroller && outer) {
       pane.addEventListener("scroll", () => {
-        const scroll =  pane.scrollTop;
+        const scroll = pane.scrollTop;
         const isTop = scroll < 50;
-        const isMiddle = scroll >= 50 && scroll < (scroller.scrollHeight - pane.offsetHeight) - 50;
+        const isMiddle =
+          scroll >= 50 &&
+          scroll < scroller.scrollHeight - pane.offsetHeight - 50;
 
         outer.classList.toggle("top", isTop);
         outer.classList.toggle("middle", isMiddle);
         outer.classList.toggle("bottom", !isTop && !isMiddle);
       });
     }
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -137,7 +244,8 @@ export default {
   overflow-x: hidden;
   height: 370px;
 }
-.scrollable-pane:after, .scrollable-pane:before {
+.scrollable-pane:after,
+.scrollable-pane:before {
   height: 50px;
   content: "";
   width: calc(100%);
@@ -156,10 +264,12 @@ export default {
   bottom: 0;
   margin-bottom: -1px;
 }
-.bottom.scrollable-pane:after, .top.scrollable-pane:before {
+.bottom.scrollable-pane:after,
+.top.scrollable-pane:before {
   height: 0;
 }
-.bottom .arrow-down, .top .arrow-up {
+.bottom .arrow-down,
+.top .arrow-up {
   opacity: 0;
 }
 .project {
@@ -182,7 +292,7 @@ export default {
 .description {
   display: flex;
   flex-wrap: wrap;
-  padding: .75rem;
+  padding: 0.75rem;
   width: 100%;
 }
 .projectname {
@@ -197,7 +307,8 @@ export default {
 .visit {
   align-self: flex-end;
 }
-.arrow-down, .arrow-up {
+.arrow-down,
+.arrow-up {
   position: absolute;
   right: calc(50%);
   transition: ease 500ms;
